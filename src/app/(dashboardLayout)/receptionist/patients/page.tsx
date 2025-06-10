763d721cf8616c7195ebe93394c4a7639fb642db
sakibpro2003 <sakibprodhan2003@gmail.com>
2025-06-10 13:31:50 +0600

    fix:loader changed

--- src/app/(dashboardLayout)/receptionist/patients/page.tsx
+++ src/app/(dashboardLayout)/receptionist/patients/page.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
+"use client";
+import Loader from "@/components/shared/Loader";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { IPatient, useGetAllPatientQuery } from "@/redux/features/patient/patientApi";
+import React from "react";
 
 const Patients = () => {
-    return (
-        <div>
-            all pats
-        </div>
-    );
+  const { data, isLoading, error } = useGetAllPatientQuery();
+
+
+  const patients = data?.data?.result ?? [];
+
+  if (isLoading) return <Loader></Loader>;
+  if (error) return <p>Failed to load patients.</p>;
+  if (patients.length === 0) return <p>No patients found.</p>;
+
+  return (
+    <Table>
+      <TableCaption>View All Registered Patients</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Name</TableHead>
+          <TableHead>Email</TableHead>
+          <TableHead>Phone</TableHead>
+          <TableHead>Gender</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {patients.map((patient: IPatient) => (
+          <TableRow key={patient._id}>
+            <TableCell className="font-medium">{patient.name}</TableCell>
+            <TableCell>{patient.email}</TableCell>
+            <TableCell>{patient.phone}</TableCell>
+            <TableCell>{patient.gender}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 };
 
 export default Patients;
--- src/redux/features/patient/patientApi.ts
+++ src/redux/features/patient/patientApi.ts
@@ -1,14 +1,46 @@
 import { baseApi } from "@/redux/api/baseApi";
 
+export interface IEmergencyContact {
+  emergencyContactName: string;
+  relationship: string;
+  emergencyContactPhone: string;
+}
+export interface IPatient extends Document {
+    _id:string,
+  name: string;
+  role: string;
+  email?: string;
+  phone: string;
+  releaseStatus: boolean;
+  dateOfBirth: string;
+  gender: "male" | "female" | "other";
+  address: string;
+  bloodGroup?: "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "O+" | "O-";
+  maritalStatus?: "single" | "married" | "divorced" | "widowed";
+  emergencyContact?: IEmergencyContact;
+  occupation?: string;
+  medicalHistory?: string[];
+  allergies?: string[];
+  currentMedications?: string[];
+  createdBy: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+interface GetAllPatientResponse {
+  data: {
+    result: IPatient[];
+  };
+  message: string;
+  success: boolean;
+}
+
 const patientApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    getAllPatient: builder.mutation({
-      query: () => ({
-        url: "/patient/all-patient",
-        method: "GET",
-      }),
+    getAllPatient: builder.query<GetAllPatientResponse, void>({
+      query: () => "/patient/all-patient",
     }),
   }),
 });
 
-export const { useGetAllPatientMutation } = patientApi;
+export const { useGetAllPatientQuery } = patientApi;
