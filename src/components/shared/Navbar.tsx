fee717b13e068ecb94531f77fdad2723f3c12665
sakibpro2003 <sakibprodhan2003@gmail.com>
2025-09-16 13:05:56 +0600

    feat: shadCN avatar installed

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -8,6 +8,7 @@ import { useAppDispatch } from "@/redux/hooks";
 import { logOut } from "@/redux/features/auth/authSlice";
 import { useRouter } from "next/navigation";
 import { useLogoutUserMutation } from "@/redux/features/auth/authApi";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Navbar = () => {
   const dispatch = useAppDispatch();
@@ -50,7 +51,6 @@ const Navbar = () => {
     { name: "Contact", path: "/contact" },
     { name: "Medicine Store", path: "/medicine" },
     { name: "Cart", path: "/cart" },
-
     { name: "Login", path: "/login" },
     { name: "Signup", path: "/register" },
   ];
@@ -66,7 +66,7 @@ const Navbar = () => {
   }
 
   return (
-    <nav className="w-11/12 mx-auto bg-white border-gray-200 dark:bg-gray-900 sticky top-0 z-50">
+    <nav className="w-11/12 mx-auto bg-blue-500  dark:bg-gray-900 sticky top-0 z-50">
       <div className="flex flex-wrap items-center justify-between mx-auto p-4">
         <Link
           href="/"
@@ -84,10 +84,11 @@ const Navbar = () => {
         )}
 
         {/* Hamburger Menu Button */}
+
         <button
           onClick={() => setIsOpen(!isOpen)}
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
         >
           <span className="sr-only">Open main menu</span>
           <svg
@@ -112,12 +113,12 @@ const Navbar = () => {
           className={`w-full md:block md:w-auto ${isOpen ? "block" : "hidden"}`}
           id="navbar-default"
         >
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border md:flex-row md:space-x-8 md:mt-0 md:border-0">
             {routes.map((item) => (
               <li key={item.name}>
                 <Link
                   href={item.path}
-                  className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 px-3 rounded-sm text-white hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   {item.name}
                 </Link>
@@ -127,7 +128,23 @@ const Navbar = () => {
         </div>
 
         {/* Logout Button */}
-        <Button onClick={handleLogout}>Logout</Button>
+        <div className="flex justify-end gap-2 items-center ">
+          {user ? (
+            <>
+              <Avatar className="">
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <Button onClick={handleLogout}>Logout</Button>
+            </>
+          ) : (
+            <Button>
+              <Link className="" href={"/login"}>
+                Login
+              </Link>
+            </Button>
+          )}
+        </div>
       </div>
     </nav>
   );
