b2eaeeeeac0233c94978c9964a018bcf4963f888
sakibpro2003 <sakibprodhan2003@gmail.com>
2025-09-28 22:46:02 +0600

    feat: new logo added

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-
+import React from "react";
+import logo from "../../../public/logo.png";
+import Image from "next/image";
 const Logo = () => {
-    return (
-        <div>
-            LOGO
-        </div>
-    );
+  return (
+    <div>
+      <Image height={200} width={200} alt="logo.png" src={logo}></Image>
+    </div>
+  );
 };
 
 export default Logo;
